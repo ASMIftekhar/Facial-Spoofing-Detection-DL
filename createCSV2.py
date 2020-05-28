d1863231d7678bc85e7db0090df8db904746cf9d
Raphael Ruschel <raphael251@ucsb.edu>
2020-05-28 00:33:47 -0700

    Update createCSV2.py

--- createCSV2.py
+++ createCSV2.py
@@ -35,7 +35,7 @@ with open(output_name, mode='w', newline='') as csv_file:
     writer = csv.writer(csv_file)
 
     for file in file_names:
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
         words = file.split('/')
         #codes = words[-1].split('_')[-1]
         codes = words[-1].split('_')
